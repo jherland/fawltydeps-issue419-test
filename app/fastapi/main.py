d3af6e9fd7c866551a6825436df557cf54999dda
Johan Herland <johan.herland@tweag.io>
2024-03-14 12:57:36 +0100

    Demonstrate how Python resolves fastapi import vs. actual FIRSTPARTY import

--- app/fastapi/main.py
+++ app/fastapi/main.py
@@ -1,3 +1,14 @@
-from fastapi import FastAPI
+import fastapi
 
-app = FastAPI()
+app = fastapi.FastAPI()
+
+print(f"fastapi found at {fastapi.__file__}")
+
+import same_dir_module
+
+print(f"same_dir_module found at {same_dir_module.__file__}")
+
+import sys
+
+print(f"sys.path is {sys.path}")
+
